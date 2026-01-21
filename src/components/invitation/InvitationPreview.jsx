4082d47a95ccc2c0f4c4c7248d3d35145a94edba
Ashrithhn <ashrith.sringeri@github.com>
2026-01-21 22:14:51 +0530

    add invitation preview component

---
+++ src/components/invitation/InvitationPreview.jsx
@@ -0,0 +1,10 @@
+function InvitationPreview({ title }) {
+  return (
+    <div className="invitation-preview">
+      <h2>{title}</h2>
+      <p>Preview will appear here</p>
+    </div>
+  );
+}
+
+export default InvitationPreview;
